414b6175d026dee199904f1d3bf9750d4217dd07
sameertri039 <sameertri039@gmail.com>
2023-06-28 14:35:27 +0530

    Added rate limiting in the api calls

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,11 +1,15 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
+	"url-shortener/database"
 	"url-shortener/helpers"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -30,6 +34,23 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	// Implement rate limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+
+	// Find the IP address of the user in the database
+	_, err := r2.Get(database.Ctx, c.IP()).Result()
+	// If that IP address is ot present in the database, then we need to set the IP in the database with refresh time of 30 min
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ := r2.Get(database.Ctx, c.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			r2.TTL(database.Ctx, c.IP()).Result()
+			return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Rate limit exceeded, try after some time"})
+		}
+
+	}
 
 	// Check if the URL passed by the user is an actual URL or not
 	if !govalidator.IsURL(body.URL) {
@@ -43,5 +64,9 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	// Enfore HTTPS
 	body.URL = helpers.EnforeHTTP(body.URL)
+
+	// decrement the count by 1
+	r2.Decr(database.Ctx, c.IP())
+
 	return nil
 }
