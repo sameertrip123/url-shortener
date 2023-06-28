1f44701484cbc75819243c112f63ce5499895dfe
sameertri039 <sameertri039@gmail.com>
2023-06-28 14:19:10 +0530

    Added the resolve url logic in the resolve.go file

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,35 @@
 package routes
+
+import (
+	"url-shortener/database"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short URL not found",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Cannot connect to database",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+
+	// Redirect the user to the original address with redirect status code of 301
+	return c.Redirect(value, 301)
+
+}
