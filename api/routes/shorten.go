ebe203022c0a748ad5f4f69a3ad592aafa0e9f84
sameertri039 <sameertri039@gmail.com>
2023-06-28 13:26:23 +0530

    Intial project structure

---
+++ api/routes/shorten.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
+)
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL            string        `json:"url"`
+	CustomShort    string        `json:"short"`
+	Expiry         time.Duration `json:"expiry"`
+	XRateLimiting  int           `json:"rate_limit"`
+	XRateLimitRest int           `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// Implement rate limiting
+
+	// Check if the URL passed by the user is an actual URL or not
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "invalid URL"})
+	}
+
+	// Check if the URL is a domain URL
+
+}
